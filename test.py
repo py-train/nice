6bde7c7977e4c5bb7263eb3a2834a4dc0e3ebf7e
Ashish Gulati <ashishgulati@Ashishs-MacBook-Air-3.local>
2025-09-19 07:37:50 +0530

    test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+parts = ['rpqkhuR',
+         'rRmQ5BnJAKXZ6KEX2'
+         '2EiY3j8vUIJguZHXu18KZ9GLTPdlsaOXqnjo6Sevlefa1zd1T3BlbkFJYgInPdH3Cb6EfdKsTqNzRrrhTHfRcGBDbHAamhZp',
+         '_2wC8dxDxNk8Gm6pWYG8TU07jF6sCvP8A'
+]
+
+if __name__ == '__main__':
+    print('sk-proj-', end='')
+    print('-'.join(parts))
+
